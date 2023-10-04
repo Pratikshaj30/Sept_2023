13265cfe01d9d62eb63b85f5256e0c9c86a2a2e4
Pratiksha <jpratiksha5000@gmail.com>
2023-10-04 15:49:47 +0530

    this is my 2nd commit

--- Implicit_Wait.py
+++ Implicit_Wait.py
@@ -1,4 +1,18 @@
 from selenium import webdriver
-
+from selenium.webdriver.common.by import By
 import time
+
+driver=webdriver.Chrome()
+driver.get('https://the-internet.herokuapp.com/dynamic_loading/2')
+driver.find_element(By.XPATH,'/html/body/div[2]/div/div/div/button').click()
+driver.implicitly_wait(10)
+load=driver.find_element(By.XPATH,'//*[@id="loading"]').text
+
+print(load)
+text = driver.find_element(By.XPATH, '//*[@id="finish"]/h4').text
+
+print("Text : ", text)
+
+
+
 
